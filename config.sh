9c321f8f2f029588563226556e4fb9e8f3e14f01
Marie-Helene Mai <mai.mariehelene@gmail.com>
2021-07-14 06:48:01 +0100

    core(config): add git global aliases

--- config.sh
+++ config.sh
@@ -5,3 +5,21 @@ sudo xcodebuild -license # agree with license
 xcode-select --install # install command line tool
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+git config --global user.name 'Marie-Helene Mai'
+git config --global user.email 'mai.mariehelene@gmail.com'
+
+git config --global alias.co 'checkout'
+git config --global alias.cob 'checkout -b'
+git config --global alias.br 'branch'
+git config --global alias.brm 'branch -m'
+git config --global alias.brd 'branch -d'
+git config --global alias.brD 'branch -D'
+git config --global alias.cm 'commit -m'
+git config --global alias.st 'status -sb'
+git config --global alias.ll 'log --oneline'
+git config --global alias.last 'log -1 HEAD --stat'
+git config --global alias.rv 'remote -v'
+git config --global alias.d 'diff'
+git config --global alias.gl 'config --global -l'
+git config --global alias.se '!git rev-list --all | xargs git grep -F'
