4a58a5ed4a17da49c9714eb315279eb449e03b68
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-29 11:59:59 +0100

    core(config.sh): comment android path config

--- config.sh
+++ config.sh
@@ -12,8 +12,8 @@ git config --global user.name 'Marie-Helene Mai'
 git config --global user.email 'mai.mariehelene@gmail.com'
 
 # configure android studio
-echo export ANDROID_HOME=$HOME/Library/Android/sdk >> ~/.zshrc
-echo export PATH=$PATH:$ANDROID_HOME/emulator >> ~/.zshrc
-echo export PATH=$PATH:$ANDROID_HOME/tools >> ~/.zshrc
-echo export PATH=$PATH:$ANDROID_HOME/tools/bin >> ~/.zshrc
-echo export PATH=$PATH:$ANDROID_HOME/platform-tools >> ~/.zshrc
+#echo export ANDROID_HOME=$HOME/Library/Android/sdk >> ~/.zshrc
+#echo export PATH=$PATH:$ANDROID_HOME/emulator >> ~/.zshrc
+#echo export PATH=$PATH:$ANDROID_HOME/tools >> ~/.zshrc
+#echo export PATH=$PATH:$ANDROID_HOME/tools/bin >> ~/.zshrc
+#echo export PATH=$PATH:$ANDROID_HOME/platform-tools >> ~/.zshrc
