24c046fd263b361098a65d16c354a927253c94ee
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-29 12:00:53 +0100

    core(config.sh): comment xcode tools install

--- config.sh
+++ config.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # after installing Xcode
-sudo xcodebuild -license # agree with license
-xcode-select --install # install command line tool
+#sudo xcodebuild -license # agree with license
+#xcode-select --install # install command line tool
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap Homebrew/bundle
