cf4f8a7530266bd31a47916c9fdc87dc59e56fc6
Marie-Helene Mai <mai.mariehelene@gmail.com>
2022-08-29 12:15:34 +0100

    core(git): reorder aliases; add commit all with msg

--- config.sh
+++ config.sh
@@ -4,14 +4,18 @@
 #sudo xcodebuild -license # agree with license
 #xcode-select --install # install command line tool
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew tap Homebrew/bundle
-brew bundle
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#brew tap Homebrew/bundle
+#brew bundle
+
+# /!\ This will override any existing .gitconfig located at ~/.
+# copies a default .gitconfig file with some handy aliases
+cp git/.gitconfig ~/.gitconfig
 
 git config --global user.name 'Marie-Helene Mai'
 git config --global user.email 'mai.mariehelene@gmail.com'
 
-# configure android studio
+# configures android studio
 #echo export ANDROID_HOME=$HOME/Library/Android/sdk >> ~/.zshrc
 #echo export PATH=$PATH:$ANDROID_HOME/emulator >> ~/.zshrc
 #echo export PATH=$PATH:$ANDROID_HOME/tools >> ~/.zshrc
